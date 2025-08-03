2810dddee29db66816e22f63ec944e9533f0f421
Sasha <s.marchuk95@gmail.com>
2025-08-03 23:29:21 +0200

    more habits

--- src/pages/Habits.tsx
+++ src/pages/Habits.tsx
@@ -9,6 +9,8 @@ const Habits = () => {
     });
     const [name, setName] = useState('');
     const [completed, setCompleted] = useState(false);
+    const [showForm, setShowForm] = useState(false);
+
 
     useEffect(() => {
         const saved = localStorage.getItem('habits');
@@ -21,17 +23,52 @@ const Habits = () => {
         localStorage.setItem('habits', JSON.stringify(habits));
     }, [habits]);
 
+    const handleAddHabit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const newHabit: Habit = {
+            id: habits.length + 1,
+            name,
+            completed
+        };
+
+        setHabits([...habits, newHabit]);
+        setName('');
+        setCompleted(false);
+        setShowForm(false);
+    };
 
     return (
         <div>
             <h1 className="text-2xl font-bold">📚 My Habits</h1>
+            <button
+                onClick={() => setShowForm(!showForm)}
+                className="mt-4 mb-6 bg-blue-500 text-white px-4 py-2 rounded">
+                {showForm ? 'Cancel' : 'Add Habit'}
+            </button>
+
+            {showForm && (
+                <form onSubmit={handleAddHabit} className="space-y-2 mb-8">
+                    <input
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Name"
+                        className="border p-2 w-full"
+                    />
+                    <input
+                        type="checkbox"
+                        checked={completed}
+                        onChange={() => setCompleted(!completed)}
+                    />
+                    <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded">
+                        ✅ Add
+                    </button>
+                </form>
+            )}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {habits.map((habits) => (
-                    <HabitsCard key={habits.id} habit={{
-                        id: 0,
-                        name: 'undefined',
-                        completed: false
-                    }} />
+                    <HabitsCard key={habits.id} habit={habits}
+                    />
                 ))}
             </div>
         </div>
